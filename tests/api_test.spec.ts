c19d17335df76be1045bdf0c264980d0cdf516e3
Sebastian <sebastianarzadun@gmail.com>
2024-05-28 23:12:52 -0300

    added APIs

--- tests/api_test.spec.ts
+++ tests/api_test.spec.ts
@@ -1,5 +1,29 @@
 import { test, expect } from "@playwright/test";
 
+test("API GET Request", async ({ request }) => {
+
+
+    const response = await request.get("https://reqres.in/api/users/2")
+
+    expect(response.status()).toBe(200);
+
+    const text = await response.text();
+    expect(text).toContain("Weaver");
+
+    console.log(await response.json());
+
+})
+
+test("API DELETE Request", async ({ request }) => {
+
+
+    const response = await request.delete("https://reqres.in/api/users/2")
+
+    expect(response.status()).toBe(204);
+
+
+})
+
 test("API POST Request", async ({ request }) => {
 
     const response = await request.post("https://reqres.in/api/users", {
@@ -13,6 +37,24 @@ test("API POST Request", async ({ request }) => {
 
     const text = await response.text();
     expect(text).toContain("hello");
+
+    console.log(await response.json());
+
+})
+
+test("API PUT Request", async ({ request }) => {
+
+    const response = await request.put("https://reqres.in/api/users/2", {
+        data: {
+            "name": "updatedName",
+            "job": "updatedJob"
+        }
+    })
+
+    expect(response.status()).toBe(200);
+
+    const text = await response.text();
+    expect(text).toContain("updatedName");
 
     console.log(await response.json());
 
